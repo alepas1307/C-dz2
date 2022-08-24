98f89fa233f1f7fbf0d5de5960447311b2d9fc70
ale_pashkin <alepas1307@gmail.com>
2022-08-24 23:11:45 +0300

    2_13

---
+++ 2_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int num = new Random().Next();
+Console.WriteLine(num);
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.Write("Третья цифра числа равна ");
+    Console.WriteLine(num.ToString()[2]);
+}
